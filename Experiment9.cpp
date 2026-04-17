bb30599fa9ec70d2edf4251a3995d15fd661cd08
Rohit Thakur <kumarrohit58590@gmail.com>
2026-04-17 15:42:44 +0530

    Experiment 8,9 done

---
+++ Experiment9.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string shortestCommonSupersequence(string str1, string str2) {
+    int n = str1.size(), m = str2.size();
+
+    // Step 1: Build LCS table
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (str1[i - 1] == str2[j - 1])
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+
+    // Step 2: Build SCS string
+    string result = "";
+    int i = n, j = m;
+
+    while (i > 0 && j > 0) {
+        if (str1[i - 1] == str2[j - 1]) {
+            result += str1[i - 1];
+            i--; j--;
+        }
+        else if (dp[i - 1][j] > dp[i][j - 1]) {
+            result += str1[i - 1];
+            i--;
+        }
+        else {
+            result += str2[j - 1];
+            j--;
+        }
+    }
+
+    // Add remaining characters
+    while (i > 0) {
+        result += str1[i - 1];
+        i--;
+    }
+    while (j > 0) {
+        result += str2[j - 1];
+        j--;
+    }
+
+    // Reverse since we built it backwards
+    reverse(result.begin(), result.end());
+
+    return result;
+}
+
+int main() {
+    string str1 = "abac", str2 = "cab";
+    cout << shortestCommonSupersequence(str1, str2) << endl;
+    return 0;
+}
